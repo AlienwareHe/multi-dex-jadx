302f29e0782d3e27bfaa7ebc345b2ecd40e5bc38
xunh.he <xunh.he@qunar.com>
2020-03-24 11:09:25 +0800

    仅编译源码

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# multi-dex-jadx
-分dex编译大型APK
+由于Jadx在反编译一两百MB的大型APK的时候，对内存有较高的要求，常常反编译到一半就会陷入频繁GC导致hang住的情况，解决方案一般是分Dex进行反编译，但是手工逐个输入命令最后还要合并显然很麻烦，
+
+因此写了个脚本工具自动将apk解压，分别编译每个dex文件最后通过rsync将所有反编译产出合并。
+
+
+# 参数选项
+-d 目标APK路径
+
+-o 反编译产出目录
+
+# eg
+./multi-dex-jadx.sh -d ~/code/crawl/elong_9_58_2.apk -o ./hello
---
+++ multi-dex-jadx.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+while getopts "d:o:" arg
+do
+	case $arg in
+		d)
+			apkPath=$OPTARG
+			apkName=${OPTARG##*/}
+			apkName=`basename $apkPath .apk`
+			;;
+		o)
+			outputDir=$OPTARG
+			;;
+		?)
+			echo "unsupported argument"
+	esac
+done
+
+if [ x"$apkPath" = x ];then
+	echo "hello~ please input your apk path..."
+	exit 1
+fi
+
+if [ x"$outputDir" = x ];then
+	outputDir="$apkName"_jadx 
+fi
+
+echo "source apk path:"$apkPath
+echo "source apk name:"$apkName
+echo "decompile result path:"$outputDir
+
+cp $apkPath "$apkName".zip
+
+# unzip -o "$apkName".zip -d $outputDir > /dev/null
+codePath=$outputDir"/code"
+mkdir $codePath
+
+index=1
+for file in `ls $outputDir`
+do
+	if [ x"${file##*.}" = x"dex" ];then
+		jadx -j 1 -r --no-imports -d "$codePath"_"$index" $outputDir/$file
+		# rsync中目录路径带/表示将所有文件复制到目标目录下，而非复制文件夹 
+		rsync -a "$codePath"_"$index"/ $codePath
+		# 删除临时目录
+		rm -rf. "$codePath"_"$index"
+		let index++
+	fi
+done
